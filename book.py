5c13be01e7e79246b712267d483f49278810e56a
Lucas Harris <153184395+gallery-mauler@users.noreply.github.com>
2023-12-07 17:43:29 +0700

    Create book.py

---
+++ book.py
@@ -0,0 +1,8 @@
+# book.py
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_info(self):
+        print(f"Title: {self.title}, Author: {self.author}")
